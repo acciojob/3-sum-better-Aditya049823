fbce3c9b9fbe4d9bc5c71047b825cb91c526b34d
Aditya Harde <110459473+Aditya049823@users.noreply.github.com>
2023-06-14 10:26:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	let n=arr.length;
+	let sum=arr[0]+arr[1]+arr[n-1];
+	arr.sort(function(a,b){
+	  return a-b;
+  })
+	for(let i=1;i<n;i++)
+		{
+			let j=i+1;
+			let k=n-1;
+			while(j<k)
+				{
+					let sum=arr[i]+arr[j]+arr[k];
+					if(sum<target)
+					{
+						j++;
+					}
+					else
+					{
+						k--;
+					}
+					if(Math.abs(sum-target)<Math.abs(ans-target))
+					{
+						ans=sum;
+					}
+				}
+		}
+	return ans;
 }
 
 module.exports = threeSum;
